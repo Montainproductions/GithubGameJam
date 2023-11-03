bbb28a1f698db73838bc5bf79c12724b42db4061
Joaquin De Losada <montainproductions@gmail.com>
2023-11-03 00:39:22 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# GithubGameJam
+
+Me and a group of 7 other people have joined together to work on githubs month long game jam during November and this is our project repo.
+
+Proper repo for Unreal
---
+++ Source/GithubGameJam/GithubGameJamGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GithubGameJamGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGithubGameJamGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGithubGameJamGameMode();
+};
+
+
+
